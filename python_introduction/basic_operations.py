8f3cb600281922cbdcfe1b2b19d9e5a5a776ff1d
RobertJanam <robertbjanam@gmail.com>
2025-11-08 16:27:53 +0300

    Addition, subtraction and multiplication of two numbers

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+# Program that finds the addition, subtraction and multiplication of two numbers
+number1 = 10 # first number
+number2 = 5 # second number
+
+addition = number1 + number2
+
+subtraction = number1 - number2
+
+multiplication = number1 * number2
+
+print(f"Addition of {number1} and {number2} is {addition}")
+print(f"Subtraction of {number1} and {number2} is {subtraction}")
+print(f"Multiplication of {number1} and {number2} is {multiplication}")
