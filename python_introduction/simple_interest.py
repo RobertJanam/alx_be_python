b2d7754fb2ac003c7975215875965087f5c74397
RobertJanam <robertbjanam@gmail.com>
2025-11-09 14:13:59 +0300

    Calculate simple interest on given terms

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,9 @@
+# Program that calculates the simple interest on a given principal amount, rate of interest, and time.
+
+principal = 1000
+rate = 0.05
+time = 3
+
+interest = principal * rate * time # formula
+
+print(f"The simple interest is: {interest}")
