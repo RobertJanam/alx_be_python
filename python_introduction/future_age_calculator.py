dce2829cf391d652b8e6168d7b0342e385bfcba9
RobertJanam <robertbjanam@gmail.com>
2025-11-09 14:43:47 +0300

    Changed user prompt from 'Enter your current age:' to 'How old are you?'

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -6,7 +6,7 @@ future_year = 2050
 difference_in_years = future_year - current_year # calculates the difference between 2025 and 2050
 
 print("Program calculates your age in 2050 from now")
-current_age = int(input("Enter your current age: ")) # user prompt
+current_age = int(input("How old are you? ")) # user prompt
 
 future_age = difference_in_years + current_age # adds your age to the current year
 
