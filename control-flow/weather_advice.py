53bbbe9f1a54981be15043dee30b7d52edac5d53
RobertJanam <robertbjanam@gmail.com>
2025-11-13 16:42:47 +0300

    Renamed variable and added .lower() function

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,13 +1,13 @@
-"""weather_advice program is created to provide clothing recommendations for users based on specific user inputs.
+"""weather_advice program is created to provide clothing recommendations for users based on weather conditions.
  Input examples: rainy, sunny etc."""
  
-weather_today = input("What's the weather like today? (sunny/rainy/cold): ")
+weather = input("What's the weather like today? (sunny/rainy/cold): ").lower()
 
-if weather_today == "sunny":
+if weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
-elif weather_today == "rainy":
+elif weather == "rainy":
     print("Don't forget your umbrella and a raincoat.")
-elif weather_today == "cold":
+elif weather == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I don't have recommendations for this weather.")
