85df3e41fd298636fa2c3747517b7c3876c6b57b
RobertJanam <robertbjanam@gmail.com>
2025-11-15 16:58:59 +0300

    changed print format

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -4,4 +4,4 @@ number = int(input("Enter a number to see its multiplication table: "))
 
 for y in range(1, 11):
     ans = number * y # calculation
-    print("{} * {} = {}".format(number, y, ans)) # output
+    print(f"{number} * {y} = {ans}") # output
