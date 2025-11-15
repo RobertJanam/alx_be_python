63a20f566db64418af4a5b952d5ee361c869e932
RobertJanam <robertbjanam@gmail.com>
2025-11-15 17:59:33 +0300

    modified print statements

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -7,25 +7,25 @@ time_bound = input("Is it time-bound? (yes/no): ")
 match priority:
     case "high":
         if time_bound == "yes":
-            print(f"'{task}' is a high priority task that requires immediate attention today!")
+            print(f"Reminder: '{task}' is a high priority task that requires immediate attention today!")
         elif time_bound == "no":
-            print(f"'{task}' is a high priority task but isn't time sensitive. Consider completing it when you have free time.")
+            print(f"Note: '{task}' is a high priority task but isn't time sensitive. Consider completing it when you have free time.")
         else:
             print("Invalid input. Please enter (yes/no).")
 
     case "medium":
         if time_bound == "yes":
-            print(f"'{task}' is a medium priority task but requires immediate attention due to time sensitivity.")
+            print(f"Reminder: '{task}' is a medium priority task but requires immediate attention due to time sensitivity.")
         elif time_bound == "no":
-            print(f"'{task}' is a medium priority task. Consider completing it when you have free time.")
+            print(f"Note: '{task}' is a medium priority task. Consider completing it when you have free time.")
         else:
             print("Invalid input. Please enter (yes/no).")
 
     case "low":
         if time_bound == "yes":
-            print(f"'{task}' is a low priority task but requires immediate attention due to time sensitivity.")
+            print(f"Reminder: '{task}' is a low priority task but requires immediate attention due to time sensitivity.")
         elif time_bound == "no":
-            print(f"'{task}' is a low priority task. Consider completing it when you have free time.")
+            print(f"Note: '{task}' is a low priority task. Consider completing it when you have free time.")
         else:
             print("Invalid input. Please enter (yes/no).")
     
