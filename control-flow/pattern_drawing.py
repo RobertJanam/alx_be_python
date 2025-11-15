69736e488f9fdae5baed19ff4835ab083f65d53c
RobertJanam <robertbjanam@gmail.com>
2025-11-15 17:14:45 +0300

    changed pattern

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -3,7 +3,7 @@ number = int(input("Enter the size of the pattern: "))
 limit_count = 0
 while limit_count < number:
     for i in range(number):
-        print("x", end="")
+        print("*", end="")
     limit_count += 1
     print()
     
